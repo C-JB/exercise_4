de8a6bacb41ab5e3b3b9cc605c98ec1762ca1126
c_jb <c_jean4@hotmail.com>
2014-02-01 11:37:07 -0500

    sent my second try

--- ex04_1/bin/ex04_1.dart
+++ ex04_1/bin/ex04_1.dart
@@ -2,12 +2,12 @@
    var a = 0;
    var b = 1;
    print (a);
-   print (b);{
-   var n = a + b; 
-     for (var n = 1; n < 6; n++) 
-       print(n);
+   print (b);
+   var n = a + b;{ 
+   for (var n=1; n<6; n++)
+     print(n);
      n = a + b;
-     n = a;
-     n = b;
+     a = n;
+     b = n;
    }
   }
