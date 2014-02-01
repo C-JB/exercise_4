d7cd8627db15bda88621851ceed1c0f226c57733
c_jb <c_jean4@hotmail.com>
2014-02-01 11:46:57 -0500

    add comments

--- ex04_3/bin/ex04_3.dart
+++ ex04_3/bin/ex04_3.dart
@@ -3,5 +3,8 @@ main() {
   DateTime bday = DateTime.parse("1980-06-01 12:00:00");
 
   Duration difference = today.difference(bday);
-  print(difference.inDays);
+  print((difference.inDays) + ("days"));
   }
+// how do I put two different arguments on the same line
+// example: 488 days
+// print(difference.inDays + "days");
